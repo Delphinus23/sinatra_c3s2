2ad8b6e981fe49e291b00110d47f974bc6b80bd7
My <katiebishop23@gmail.com>
2015-02-16 17:58:03 +0000

    git add bootstrap and container

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
  require 'sinatra'
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+<div class = "container">
 
 #get '/' do 
  # output  = "<form method='post' action='/'>"
@@ -38,3 +47,5 @@ post '/' do
      erb :no_drink
    end
  end
+</div>
+
